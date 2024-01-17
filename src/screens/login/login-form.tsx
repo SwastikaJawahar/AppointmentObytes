5dc909aa65f841e66812f768dec421d46662e79f
SwastikaJawahar <136087727+SwastikaJawahar@users.noreply.github.com>
2024-01-17 15:31:35 +0000

    feat(init): initialize

---
+++ src/screens/login/login-form.tsx
@@ -0,0 +1,66 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import React from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+
+import { Button, ControlledInput, Text, View } from '@/ui';
+import { Image } from '@/ui';
+const schema = z.object({
+  name: z.string().optional(),
+  email: z
+    .string({
+      required_error: 'Email is required',
+    })
+    .email('Invalid email format'),
+  password: z
+    .string({
+      required_error: 'Password is required',
+    })
+    .min(6, 'Password must be at least 6 characters'),
+});
+
+export type FormType = z.infer<typeof schema>;
+
+export type LoginFormProps = {
+  onSubmit?: SubmitHandler<FormType>;
+};
+
+export const LoginForm = ({ onSubmit = () => {} }: LoginFormProps) => {
+  const { handleSubmit, control } = useForm<FormType>({
+    resolver: zodResolver(schema),
+  });
+  return (
+    <View className="flex-1 justify-center bg-teal-50 p-4">
+      <Image
+        className="h-40"
+        source={{
+          uri: 'https://pbs.twimg.com/media/FVHpq6yXsAAP6U2?format=jpg&name=medium',
+        }}
+      />
+      <Text testID="form-title" variant="h1" className="pb-6 text-center">
+        Login
+      </Text>
+      <ControlledInput
+        testID="email-input"
+        control={control}
+        name="email"
+        label="Email"
+      />
+      <ControlledInput
+        testID="password-input"
+        control={control}
+        name="password"
+        label="Password"
+        placeholder="***"
+        secureTextEntry={true}
+      />
+      <Button
+        testID="login-button"
+        label="Login"
+        onPress={handleSubmit(onSubmit)}
+        variant="primary"
+      />
+    </View>
+  );
+};
